050cc2e4e7e2df93001472f1a1836e57cfce77e5
abdulhakim10 <s.abdulhakim10@gmail.com>
2025-11-24 14:41:09 +0800

    linked list deletion practice

---
+++ module-6/delete_at_tail.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    // constructor
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = tail->next;
+}
+
+void delete_at_any_position(Node* head, Node* &tail, int idx)
+{
+    Node* temp = head;
+    for (int i = 0; i < idx -1; i++)
+    {
+        temp = temp->next;
+        if(temp == NULL)
+        {
+            return;
+        }
+    }
+    Node* deletenode = temp->next;
+    temp->next = deletenode->next; // temp->next = temp->next->next; same
+    delete deletenode;
+    tail = temp;
+    
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int val;
+    while(true)
+    {
+        cin >> val;
+        if(val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    cout << "Before delete:" << tail->val << endl;
+    delete_at_any_position(head,tail,4);
+    cout << "After delete:" << tail->val << endl;
+    print_linked_list(head);
+}
